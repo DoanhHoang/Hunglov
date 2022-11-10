b4f6902eaf68aca5d1538c846ce2c750a10401d2
DoanhHoang <hoangngocdoanh031200@gmail.com>
2022-11-10 22:47:16 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "I Love MeoMeo",
+    introTitle: 'Gửi Lê Ngọc Phương Thảo!',
+    introDesc: `Mình có con mèo tên Chuyện vừa nhảy qua phòng bạn, bạn cho mình bắt Chuyện nhe 🥰`,
+    btnIntro: 'Ok',
+    title: 'Nay cậu có vẻ mệt mỏi và buồn lắm mình có cái này cho cậu nè ._.',
+    desc: 'Cậu nếu buồn thì cứ nói mình nè, mình luôn chờ đợi cậu á, cùng cố lên nhe, không có gì có thể đánh bại cậu đâu',
+    btnYes: 'Yêu cậu <33',
+    btnNo: 'Ngáo à :3',
+    question: 'Ngoài ra mình cũng thương bạn lắm á, bạn mãi iu mình nhe 😘',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Đó là điều dĩ nhiên luôn, trăm phần trăm yêu b',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nào',
+    btnAccept: 'Dạaaa',
+    messLink: 'https://facebook.com/messages/t/100006138467943'
+}
